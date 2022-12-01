1832a07c9c3ccf6f9c6bf629ec137284fbde1a8e
louie_gomez <kinggomez027.lg@gmail.com>
2022-11-30 22:14:21 -0500

    tables created

--- db/migrate/20221201031024_create_superheroes.rb
+++ db/migrate/20221201031024_create_superheroes.rb
@@ -1,4 +1,10 @@
 class CreateSuperheroes < ActiveRecord::Migration[6.1]
   def change
+    create_table :superheroes do |t|
+      t.string :name
+      t.string :power
+      t.integer :hero_id
+      t.timestamps
+    end
   end
 end
