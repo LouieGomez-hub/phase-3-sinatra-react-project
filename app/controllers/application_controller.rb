3cf815b292288308a8b7db1b2493aab7d341e7b8
louie_gomez <kinggomez027.lg@gmail.com>
2022-12-01 12:55:23 -0500

    get requests for superheros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,15 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/superheros' do
+    superheros = Superheros.all
+    superheros.to_json
   end
 
+  get '/superheros/:id' do
+    superhero = Superhero.find(params[:id])
+    superhero.to_json(include: :comments)
+  end
+
+  
 end
