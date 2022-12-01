5209ee4a3f0e40104f6b54e7fb2d094dcef5e0bc
louie_gomez <kinggomez027.lg@gmail.com>
2022-12-01 11:44:06 -0500

    migrations ran and loaded into schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_12_01_163859) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "superhero_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "superheros", force: :cascade do |t|
+    t.string "name"
+    t.string "power"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
